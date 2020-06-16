9063e170e4447aeff5c8ea4188d5253da2713065
Niraj Kamdar <nirajkamdar27@pm.me>
2020-06-16 19:31:53 +0530

    update action script

--- entrypoint.py
+++ entrypoint.py
@@ -1,12 +1,20 @@
 import os
 import subprocess
+import logging
+
+if not (os.getenv("TWINE_USERNAME") and os.getenv("TWINE_PASSWORD")):
+    raise EnvironmentError("You have to set your TWINE_USERNAME and TWINE_PASSWORD as environment variable")
 
 versions = {"3.6": "cp36-cp36m", "3.7": "cp37-cp37m", "3.8": "cp38-cp38", "3.9": "cp39-cp39"}
 for version in versions:
-    print(f"Building wheel for Python {version}")
+    logging.info(f"Building wheel for Python {version}")
     subprocess.call([f"/opt/python/{versions[version]}/bin/python", "setup.py", "bdist_wheel"])
 
-print("Repairing wheels")
+logging.info("Repairing wheels")
 for file in os.listdir("dist"):
     if os.path.isfile(os.path.join(os.getcwd(), "dist", file)):
         subprocess.call(["auditwheel", "repair", os.path.join(os.getcwd(), "dist", file)])
+
+
+logging.info("Uploading built binary to PyPI")
+subprocess.call(["twine", "upload", "wheelhouse/*"])
