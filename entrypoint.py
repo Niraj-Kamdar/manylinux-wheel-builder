359cf12811e7e3fa05081f39b7f4cd877210aeb4
Niraj Kamdar <nirajkamdar27@pm.me>
2020-07-20 19:44:00 +0530

    Add option to choose python version user want to build

--- entrypoint.py
+++ entrypoint.py
@@ -1,20 +1,32 @@
+import logging
 import os
 import subprocess
-import logging
+import sys
 
-if not (os.getenv("TWINE_USERNAME") and os.getenv("TWINE_PASSWORD")):
-    raise EnvironmentError("You have to set your TWINE_USERNAME and TWINE_PASSWORD as environment variable")
+VERSIONS = {"3.6": "cp36-cp36m", "3.7": "cp37-cp37m", "3.8": "cp38-cp38", "3.9": "cp39-cp39"}
 
-versions = {"3.6": "cp36-cp36m", "3.7": "cp37-cp37m", "3.8": "cp38-cp38", "3.9": "cp39-cp39"}
-for version in versions:
-    logging.info(f"Building wheel for Python {version}")
-    subprocess.call([f"/opt/python/{versions[version]}/bin/python", "setup.py", "bdist_wheel"])
+if __name__ == "__main__":
 
-logging.info("Repairing wheels")
-for file in os.listdir("dist"):
-    if os.path.isfile(os.path.join(os.getcwd(), "dist", file)):
-        subprocess.call(["auditwheel", "repair", os.path.join(os.getcwd(), "dist", file)])
+    args = sys.argv
+    passed_versions_str = args[1]
+    if not (os.getenv("TWINE_USERNAME") and os.getenv("TWINE_PASSWORD")):
+        raise EnvironmentError("You have to set your TWINE_USERNAME and TWINE_PASSWORD as environment variable")
 
+    if passed_versions_str == "3.*":
+        versions = VERSIONS.keys()
+    else:
+        versions = []
+        for version in passed_versions_str.split(","):
+            versions.append(version.strip())
 
-logging.info("Uploading built binary to PyPI")
-subprocess.call(["/opt/python/cp37-cp37m/bin/python", "-m", "twine", "upload", "wheelhouse/*"])
+    for version in versions:
+        logging.info(f"Building wheel for Python {version}")
+        subprocess.call([f"/opt/python/{VERSIONS[version]}/bin/python", "setup.py", "bdist_wheel"])
+
+    logging.info("Repairing wheels")
+    for file in os.listdir("dist"):
+        if os.path.isfile(os.path.join(os.getcwd(), "dist", file)):
+            subprocess.call(["auditwheel", "repair", os.path.join(os.getcwd(), "dist", file)])
+
+    logging.info("Uploading built binaries to PyPI")
+    subprocess.call(["/opt/python/cp37-cp37m/bin/python", "-m", "twine", "upload", "wheelhouse/*"])
